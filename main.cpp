84cf5fe80e0d1a5712da33d49f4b8ab29c2b547b
AHackIt23 <hack.amkia@gmail.com>
2026-07-10 17:50:16 -0400

    update to main function
    started working on k means and main functionality and commits did not save before closing window; I am rebuilding the program now

--- main.cpp
+++ main.cpp
@@ -1,1 +1,9 @@
+int main(int argc, char** argc) {
+  if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_TIMER) != 0) return -1;
 
+SDL* window = SLD_CreateWindow("Get Your Palette Here!", SDL_WINDOWPOS_CENTERE, 
+                                SDL_WINDOWPOS_CENTERED< 1200, 780, SDL_WINDOW_RESIZEABLE);
+
+IMGUI_CHECKVERSION();
+ImGui::CreateContext();
+ImGui_ImplSDL2_InitForSDLRender(window, renderer);
