ef75b14e3594829eeaf6fb03e3b6a7f7ba63158c
AHackIt23 <hack.amkia@gmail.com>
2026-07-13 09:01:25 +0000

    ..

---
+++ build/CMakeFiles/P2_GetYourPaletteHere.dir/src/imgui/imgui_impl_sdlrenderer2.cpp.o.d
@@ -0,0 +1,115 @@
+CMakeFiles/P2_GetYourPaletteHere.dir/src/imgui/imgui_impl_sdlrenderer2.cpp.o: \
+ /workspaces/P2_GetYourPaletteHere/src/imgui/imgui_impl_sdlrenderer2.cpp \
+ /usr/include/stdc-predef.h \
+ /workspaces/P2_GetYourPaletteHere/include/imgui/imgui.h \
+ /workspaces/P2_GetYourPaletteHere/include/imgui/imconfig.h \
+ /usr/lib/gcc/x86_64-linux-gnu/13/include/float.h \
+ /usr/lib/gcc/x86_64-linux-gnu/13/include/stdarg.h \
+ /usr/lib/gcc/x86_64-linux-gnu/13/include/stddef.h /usr/include/string.h \
+ /usr/include/x86_64-linux-gnu/bits/libc-header-start.h \
+ /usr/include/features.h /usr/include/features-time64.h \
+ /usr/include/x86_64-linux-gnu/bits/wordsize.h \
+ /usr/include/x86_64-linux-gnu/bits/timesize.h \
+ /usr/include/x86_64-linux-gnu/sys/cdefs.h \
+ /usr/include/x86_64-linux-gnu/bits/long-double.h \
+ /usr/include/x86_64-linux-gnu/gnu/stubs.h \
+ /usr/include/x86_64-linux-gnu/gnu/stubs-64.h \
+ /usr/include/x86_64-linux-gnu/bits/types/locale_t.h \
+ /usr/include/x86_64-linux-gnu/bits/types/__locale_t.h \
+ /usr/include/strings.h /usr/include/assert.h \
+ /workspaces/P2_GetYourPaletteHere/include/imgui/imgui_impl_sdlrenderer2.h \
+ /usr/lib/gcc/x86_64-linux-gnu/13/include/stdint.h /usr/include/stdint.h \
+ /usr/include/x86_64-linux-gnu/bits/types.h \
+ /usr/include/x86_64-linux-gnu/bits/typesizes.h \
+ /usr/include/x86_64-linux-gnu/bits/time64.h \
+ /usr/include/x86_64-linux-gnu/bits/wchar.h \
+ /usr/include/x86_64-linux-gnu/bits/stdint-intn.h \
+ /usr/include/x86_64-linux-gnu/bits/stdint-uintn.h \
+ /usr/include/x86_64-linux-gnu/bits/stdint-least.h \
+ /workspaces/P2_GetYourPaletteHere/include/SDL2/SDL.h \
+ /workspaces/P2_GetYourPaletteHere/include/SDL2/SDL_main.h \
+ /workspaces/P2_GetYourPaletteHere/include/SDL2/SDL_stdinc.h \
+ /workspaces/P2_GetYourPaletteHere/include/SDL2/SDL_config.h \
+ /workspaces/P2_GetYourPaletteHere/include/SDL2/SDL_platform.h \
+ /workspaces/P2_GetYourPaletteHere/include/SDL2/begin_code.h \
+ /workspaces/P2_GetYourPaletteHere/include/SDL2/close_code.h \
+ /workspaces/P2_GetYourPaletteHere/include/SDL2/SDL_config_minimal.h \
+ /workspaces/P2_GetYourPaletteHere/include/SDL2/SDL_assert.h \
+ /workspaces/P2_GetYourPaletteHere/include/SDL2/SDL_atomic.h \
+ /workspaces/P2_GetYourPaletteHere/include/SDL2/SDL_audio.h \
+ /workspaces/P2_GetYourPaletteHere/include/SDL2/SDL_error.h \
+ /workspaces/P2_GetYourPaletteHere/include/SDL2/SDL_endian.h \
+ /usr/include/endian.h /usr/include/x86_64-linux-gnu/bits/endian.h \
+ /usr/include/x86_64-linux-gnu/bits/endianness.h \
+ /usr/include/x86_64-linux-gnu/bits/byteswap.h \
+ /usr/include/x86_64-linux-gnu/bits/uintn-identity.h \
+ /workspaces/P2_GetYourPaletteHere/include/SDL2/SDL_mutex.h \
+ /workspaces/P2_GetYourPaletteHere/include/SDL2/SDL_thread.h \
+ /workspaces/P2_GetYourPaletteHere/include/SDL2/SDL_rwops.h \
+ /workspaces/P2_GetYourPaletteHere/include/SDL2/SDL_clipboard.h \
+ /workspaces/P2_GetYourPaletteHere/include/SDL2/SDL_cpuinfo.h \
+ /usr/lib/gcc/x86_64-linux-gnu/13/include/mmintrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/13/include/xmmintrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/13/include/mm_malloc.h \
+ /usr/include/c++/13/stdlib.h /usr/include/c++/13/cstdlib \
+ /usr/include/x86_64-linux-gnu/c++/13/bits/c++config.h \
+ /usr/include/x86_64-linux-gnu/c++/13/bits/os_defines.h \
+ /usr/include/x86_64-linux-gnu/c++/13/bits/cpu_defines.h \
+ /usr/include/c++/13/pstl/pstl_config.h /usr/include/stdlib.h \
+ /usr/include/x86_64-linux-gnu/bits/waitflags.h \
+ /usr/include/x86_64-linux-gnu/bits/waitstatus.h \
+ /usr/include/x86_64-linux-gnu/bits/floatn.h \
+ /usr/include/x86_64-linux-gnu/bits/floatn-common.h \
+ /usr/include/x86_64-linux-gnu/sys/types.h \
+ /usr/include/x86_64-linux-gnu/bits/types/clock_t.h \
+ /usr/include/x86_64-linux-gnu/bits/types/clockid_t.h \
+ /usr/include/x86_64-linux-gnu/bits/types/time_t.h \
+ /usr/include/x86_64-linux-gnu/bits/types/timer_t.h \
+ /usr/include/x86_64-linux-gnu/sys/select.h \
+ /usr/include/x86_64-linux-gnu/bits/select.h \
+ /usr/include/x86_64-linux-gnu/bits/types/sigset_t.h \
+ /usr/include/x86_64-linux-gnu/bits/types/__sigset_t.h \
+ /usr/include/x86_64-linux-gnu/bits/types/struct_timeval.h \
+ /usr/include/x86_64-linux-gnu/bits/types/struct_timespec.h \
+ /usr/include/x86_64-linux-gnu/bits/pthreadtypes.h \
+ /usr/include/x86_64-linux-gnu/bits/thread-shared-types.h \
+ /usr/include/x86_64-linux-gnu/bits/pthreadtypes-arch.h \
+ /usr/include/x86_64-linux-gnu/bits/atomic_wide_counter.h \
+ /usr/include/x86_64-linux-gnu/bits/struct_mutex.h \
+ /usr/include/x86_64-linux-gnu/bits/struct_rwlock.h /usr/include/alloca.h \
+ /usr/include/x86_64-linux-gnu/bits/stdlib-float.h \
+ /usr/include/c++/13/bits/std_abs.h \
+ /usr/lib/gcc/x86_64-linux-gnu/13/include/emmintrin.h \
+ /workspaces/P2_GetYourPaletteHere/include/SDL2/SDL_events.h \
+ /workspaces/P2_GetYourPaletteHere/include/SDL2/SDL_video.h \
+ /workspaces/P2_GetYourPaletteHere/include/SDL2/SDL_pixels.h \
+ /workspaces/P2_GetYourPaletteHere/include/SDL2/SDL_rect.h \
+ /workspaces/P2_GetYourPaletteHere/include/SDL2/SDL_surface.h \
+ /workspaces/P2_GetYourPaletteHere/include/SDL2/SDL_blendmode.h \
+ /workspaces/P2_GetYourPaletteHere/include/SDL2/SDL_keyboard.h \
+ /workspaces/P2_GetYourPaletteHere/include/SDL2/SDL_keycode.h \
+ /workspaces/P2_GetYourPaletteHere/include/SDL2/SDL_scancode.h \
+ /workspaces/P2_GetYourPaletteHere/include/SDL2/SDL_mouse.h \
+ /workspaces/P2_GetYourPaletteHere/include/SDL2/SDL_joystick.h \
+ /workspaces/P2_GetYourPaletteHere/include/SDL2/SDL_guid.h \
+ /workspaces/P2_GetYourPaletteHere/include/SDL2/SDL_gamecontroller.h \
+ /workspaces/P2_GetYourPaletteHere/include/SDL2/SDL_sensor.h \
+ /workspaces/P2_GetYourPaletteHere/include/SDL2/SDL_quit.h \
+ /workspaces/P2_GetYourPaletteHere/include/SDL2/SDL_gesture.h \
+ /workspaces/P2_GetYourPaletteHere/include/SDL2/SDL_touch.h \
+ /workspaces/P2_GetYourPaletteHere/include/SDL2/SDL_filesystem.h \
+ /workspaces/P2_GetYourPaletteHere/include/SDL2/SDL_haptic.h \
+ /workspaces/P2_GetYourPaletteHere/include/SDL2/SDL_hidapi.h \
+ /workspaces/P2_GetYourPaletteHere/include/SDL2/SDL_hints.h \
+ /workspaces/P2_GetYourPaletteHere/include/SDL2/SDL_loadso.h \
+ /workspaces/P2_GetYourPaletteHere/include/SDL2/SDL_log.h \
+ /workspaces/P2_GetYourPaletteHere/include/SDL2/SDL_messagebox.h \
+ /workspaces/P2_GetYourPaletteHere/include/SDL2/SDL_metal.h \
+ /workspaces/P2_GetYourPaletteHere/include/SDL2/SDL_power.h \
+ /workspaces/P2_GetYourPaletteHere/include/SDL2/SDL_render.h \
+ /workspaces/P2_GetYourPaletteHere/include/SDL2/SDL_shape.h \
+ /workspaces/P2_GetYourPaletteHere/include/SDL2/SDL_system.h \
+ /workspaces/P2_GetYourPaletteHere/include/SDL2/SDL_timer.h \
+ /workspaces/P2_GetYourPaletteHere/include/SDL2/SDL_version.h \
+ /workspaces/P2_GetYourPaletteHere/include/SDL2/SDL_locale.h \
+ /workspaces/P2_GetYourPaletteHere/include/SDL2/SDL_misc.h
