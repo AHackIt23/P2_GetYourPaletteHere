66736ea4309110d46591f4fe3f9ed534db9169a6
AHackIt23 <hack.amkia@gmail.com>
2026-07-13 05:04:12 +0000

    edits

---
+++ build/CMakeFiles/P2_GetYourPaletteHere.dir/DependInfo.cmake
@@ -0,0 +1,33 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/workspaces/P2_GetYourPaletteHere/src/Color.cpp" "CMakeFiles/P2_GetYourPaletteHere.dir/src/Color.cpp.o" "gcc" "CMakeFiles/P2_GetYourPaletteHere.dir/src/Color.cpp.o.d"
+  "/workspaces/P2_GetYourPaletteHere/src/MedianCut.cpp" "CMakeFiles/P2_GetYourPaletteHere.dir/src/MedianCut.cpp.o" "gcc" "CMakeFiles/P2_GetYourPaletteHere.dir/src/MedianCut.cpp.o.d"
+  "/workspaces/P2_GetYourPaletteHere/src/imgui/imgui.cpp" "CMakeFiles/P2_GetYourPaletteHere.dir/src/imgui/imgui.cpp.o" "gcc" "CMakeFiles/P2_GetYourPaletteHere.dir/src/imgui/imgui.cpp.o.d"
+  "/workspaces/P2_GetYourPaletteHere/src/imgui/imgui_demo.cpp" "CMakeFiles/P2_GetYourPaletteHere.dir/src/imgui/imgui_demo.cpp.o" "gcc" "CMakeFiles/P2_GetYourPaletteHere.dir/src/imgui/imgui_demo.cpp.o.d"
+  "/workspaces/P2_GetYourPaletteHere/src/imgui/imgui_draw.cpp" "CMakeFiles/P2_GetYourPaletteHere.dir/src/imgui/imgui_draw.cpp.o" "gcc" "CMakeFiles/P2_GetYourPaletteHere.dir/src/imgui/imgui_draw.cpp.o.d"
+  "/workspaces/P2_GetYourPaletteHere/src/imgui/imgui_impl_opengl3.cpp" "CMakeFiles/P2_GetYourPaletteHere.dir/src/imgui/imgui_impl_opengl3.cpp.o" "gcc" "CMakeFiles/P2_GetYourPaletteHere.dir/src/imgui/imgui_impl_opengl3.cpp.o.d"
+  "/workspaces/P2_GetYourPaletteHere/src/imgui/imgui_impl_sdl2.cpp" "CMakeFiles/P2_GetYourPaletteHere.dir/src/imgui/imgui_impl_sdl2.cpp.o" "gcc" "CMakeFiles/P2_GetYourPaletteHere.dir/src/imgui/imgui_impl_sdl2.cpp.o.d"
+  "/workspaces/P2_GetYourPaletteHere/src/imgui/imgui_impl_sdlrenderer2.cpp" "CMakeFiles/P2_GetYourPaletteHere.dir/src/imgui/imgui_impl_sdlrenderer2.cpp.o" "gcc" "CMakeFiles/P2_GetYourPaletteHere.dir/src/imgui/imgui_impl_sdlrenderer2.cpp.o.d"
+  "/workspaces/P2_GetYourPaletteHere/src/imgui/imgui_tables.cpp" "CMakeFiles/P2_GetYourPaletteHere.dir/src/imgui/imgui_tables.cpp.o" "gcc" "CMakeFiles/P2_GetYourPaletteHere.dir/src/imgui/imgui_tables.cpp.o.d"
+  "/workspaces/P2_GetYourPaletteHere/src/imgui/imgui_widgets.cpp" "CMakeFiles/P2_GetYourPaletteHere.dir/src/imgui/imgui_widgets.cpp.o" "gcc" "CMakeFiles/P2_GetYourPaletteHere.dir/src/imgui/imgui_widgets.cpp.o.d"
+  "/workspaces/P2_GetYourPaletteHere/src/main.cpp" "CMakeFiles/P2_GetYourPaletteHere.dir/src/main.cpp.o" "gcc" "CMakeFiles/P2_GetYourPaletteHere.dir/src/main.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
